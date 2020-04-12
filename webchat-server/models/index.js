7f522970cf8417e9bd170d4b12b33f0eb22c1ea1
Wu-jiaming <33871323+Wu-jiaming@users.noreply.github.com>
2020-04-12 14:22:30 +0800

    改数据库名

--- webchat-server/models/index.js
+++ webchat-server/models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 mongoose.Promise = require("bluebird")
-mongoose.connect('mongodb://localhost:27017/chat')
+mongoose.connect('mongodb://localhost:27017/webchat')
 
 module.exports = {
     User: require("./user"),
